e3f4fd6787c4a86419497026102993fff58987f5
Yodwaae <lucas.spitz.drevet@gmail.com>
2024-10-27 16:48:18 +0100

    raycast for player collision

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,22 +1,29 @@
 extends Node2D
 
+#region ===== VARIABLES INITIALISATION =======
+
+# === RAYCAST ===
+@onready var raycastLeft: RayCast2D = %raycastLeft
+@onready var raycastRight: RayCast2D = %raycastRight
+@onready var raycastDown: RayCast2D = %raycastDown
+@onready var raycastUp: RayCast2D = %raycastUp
+
+# === MOVEMENTS ===
 # No need to export, will be modified depending on tile size
 var distance : int = 64
+#endregion
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	
-	# Movements Inputs
-	if Input.is_action_just_pressed("moveDown"):
+	# ===== MOVEMENTS ======
+	
+	# NOTE Using elif to avoid moving diagonaly almost instantly when two inputs are pressed simultaneously, 
+	# which can cause problems with the collision detection
+	if Input.is_action_just_pressed("moveDown") and not raycastDown.is_colliding() :
 		move_local_y(distance)
-	if Input.is_action_just_pressed("moveUp"):
+	elif Input.is_action_just_pressed("moveUp") and not raycastUp.is_colliding():
 		move_local_y(-distance)
-	if Input.is_action_just_pressed("moveRight"):
+	elif Input.is_action_just_pressed("moveRight") and not raycastRight.is_colliding():
 		move_local_x(distance)
-	if Input.is_action_just_pressed("moveLeft"):
+	elif Input.is_action_just_pressed("moveLeft") and not raycastLeft.is_colliding():
 		move_local_x(-distance)
