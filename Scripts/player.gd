a40fb31bf2f0333263a7dd4ad2c2196e3de625f5
Yodwaae <lucas.spitz.drevet@gmail.com>
2024-10-30 13:00:06 +0100

    Refactoring of the player movements
    Player script :
    - Collision check now decomposed in multiple boolean and managed by helper functions
    - Removed previous boolean that managed canMove
    - Replaced the raycast2D with rectangular shapecast2D
    - Deleted the movement functions as they were just useless wrapper that obfuscated the code
    
    TODO :
    - Create array of raycasts for each direction
    - Refactor the multiple helper function (one for each direction) into one general function that take as an argument the array of raycasts for the desired direfction

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,18 +3,17 @@ extends Node2D
 #region ===== VARIABLES INITIALISATION =======
 
 # === RAYCAST ===
-
 # Obstacles Raycasts
-@onready var obsRaycastDown: RayCast2D = %obsRaycastDown
-@onready var obsRaycastUp: RayCast2D = %osbRaycastUp
-@onready var obsRaycastRight: RayCast2D = %obsRaycastRight
-@onready var obsRaycastLeft: RayCast2D = %obsRaycastLeft
+@onready var obsRaycastDown: ShapeCast2D = %obsRaycastDown
+@onready var obsRaycastUp: ShapeCast2D = %obsRaycastUp
+@onready var obsRaycastRight: ShapeCast2D = %obsRaycastRight
+@onready var obsRaycastLeft: ShapeCast2D = %obsRaycastLeft
 
 # Boxes Raycasts 
-@onready var boxRaycastDown: RayCast2D = %boxRaycastDown
-@onready var boxRaycastUp: RayCast2D = %boxRaycastUp
-@onready var boxRaycastRight: RayCast2D = %boxRaycastRight
-@onready var boxRaycastLeft: RayCast2D = %boxRaycastLeft
+@onready var boxRaycastDown: ShapeCast2D = %boxRaycastDown
+@onready var boxRaycastUp: ShapeCast2D = %boxRaycastUp
+@onready var boxRaycastRight: ShapeCast2D = %boxRaycastRight
+@onready var boxRaycastLeft: ShapeCast2D = %boxRaycastLeft
 
 # After Boxes Raycasts
 @onready var afterBoxRaycastDown: ShapeCast2D = %afterBoxRaycastDown
@@ -22,52 +21,93 @@ extends Node2D
 @onready var afterBoxRaycastRight: ShapeCast2D = %afterBoxRaycastRight
 @onready var afterBoxRaycastLeft: ShapeCast2D = %afterBoxRaycastLeft
 
+# Raycasts group by direction TODO
 
 # === MOVEMENTS ===
 # No need to export, will be modified depending on tile size
 var moveLength : int = 64
-var canMoveDown : bool = false
-var canMoveUp : bool = false
-var canMoveLeft : bool = false
-var canMoveRight : bool = false
 
 #endregion
 
 
 #region ===== FONCTIONS =====
 
-func moveDown(distance):
-	move_local_y(distance)
+func canMoveDown() -> bool:
 	
-func moveUp(distance):
-	move_local_y(-distance)
+	# Check the raycast collisions
+	var isThereObstacle = obsRaycastDown.is_colliding()
+	var isThereBox = boxRaycastDown.is_colliding()
+	var isThereSomethingAfterBox = afterBoxRaycastDown.is_colliding()
 	
-func moveRight(distance):
-	move_local_x(distance)
+	# Get the reference to the box
+	if isThereBox:
+		var box = boxRaycastDown.get_collider(0)
+		print(box.get_parent())
 	
-func moveLeft(distance):
-	move_local_x(-distance)
+	# Player can move if there is no obstacle, no box or a box and nothing in the cell after the box
+	var canMove = not isThereObstacle and (not isThereBox or (isThereBox and not isThereSomethingAfterBox))
+	return canMove
+
+func canMoveUp() -> bool:
+	
+	# Check the raycast collisions
+	var isThereObstacle = obsRaycastUp.is_colliding()
+	var isThereBox = boxRaycastUp.is_colliding()
+	var isThereSomethingAfterBox = afterBoxRaycastUp.is_colliding()
+	
+	# Get the reference to the box
+	if isThereBox:
+		var box = boxRaycastUp.get_collider(0)
+		print(box.get_parent())
+	
+	# Player can move if there is no obstacle, no box or a box and nothing in the cell after the box
+	var canMove = not isThereObstacle and (not isThereBox or (isThereBox and not isThereSomethingAfterBox))
+	return canMove
+
+func canMoveRight() -> bool:
+	
+	# Check the raycast collisions
+	var isThereObstacle = obsRaycastRight.is_colliding()
+	var isThereBox = boxRaycastRight.is_colliding()
+	var isThereSomethingAfterBox = afterBoxRaycastRight.is_colliding()
+	
+	# Get the reference to the box
+	if isThereBox:
+		var box = boxRaycastRight.get_collider(0)
+		print(box.get_parent())
+	
+	# Player can move if there is no obstacle, no box or a box and nothing in the cell after the box
+	var canMove = not isThereObstacle and (not isThereBox or (isThereBox and not isThereSomethingAfterBox))
+	return canMove
+	
+func canMoveLeft() -> bool:
+	
+	# Check the raycast collisions
+	var isThereObstacle = obsRaycastLeft.is_colliding()
+	var isThereBox = boxRaycastLeft.is_colliding()
+	var isThereSomethingAfterBox = afterBoxRaycastLeft.is_colliding()
+	
+	# Get the reference to the box
+	if isThereBox:
+		var box = boxRaycastLeft.get_collider(0)
+		print(box.get_parent())
+	
+	# Player can move if there is no obstacle, no box or a box and nothing in the cell after the box
+	var canMove = not isThereObstacle and (not isThereBox or (isThereBox and not isThereSomethingAfterBox))
+	return canMove
 
 func _physics_process(delta: float) -> void:
-	
-	canMoveDown = not obsRaycastDown.is_colliding() and (not boxRaycastDown.is_colliding() or (boxRaycastDown.is_colliding() and not afterBoxRaycastDown.is_colliding()))
-	canMoveUp = not obsRaycastUp.is_colliding() and (not boxRaycastUp.is_colliding() or (boxRaycastUp.is_colliding() and not afterBoxRaycastUp.is_colliding()))
-	canMoveRight = not obsRaycastRight.is_colliding() and (not boxRaycastRight.is_colliding() or (boxRaycastRight.is_colliding() and not afterBoxRaycastRight.is_colliding()))
-	canMoveLeft = not obsRaycastLeft.is_colliding() and (not boxRaycastLeft.is_colliding() or (boxRaycastLeft.is_colliding() and not afterBoxRaycastLeft.is_colliding()))
 	
 	# ===== MOVEMENTS ======
 	# NOTE Using elif to avoid moving diagonaly almost instantly when two inputs are pressed simultaneously, 
 	# which can cause problems with the collision detection
-	if Input.is_action_just_pressed("moveDown") and canMoveDown:
-		moveDown(moveLength)
-	elif Input.is_action_just_pressed("moveUp") and canMoveUp:
-		moveUp(moveLength)
-	elif Input.is_action_just_pressed("moveRight") and canMoveRight:
-		moveRight(moveLength)
-	elif Input.is_action_just_pressed("moveLeft") and canMoveLeft:
-		moveLeft(moveLength)
-		
-	if Input.is_action_just_pressed("debug"):
-		print(canMoveLeft)
+	if Input.is_action_just_pressed("moveDown") and canMoveDown():
+		move_local_y(moveLength)
+	elif Input.is_action_just_pressed("moveUp") and canMoveUp():
+		move_local_y(-moveLength)
+	elif Input.is_action_just_pressed("moveRight") and canMoveRight():
+		move_local_x(moveLength)
+	elif Input.is_action_just_pressed("moveLeft") and canMoveLeft():
+		move_local_x(-moveLength)
 
 #endregion
