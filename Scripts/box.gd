ada92215f7c50cf438102f376de1994213ed9c39
Yodwaae <lucas.spitz.drevet@gmail.com>
2024-11-09 16:51:25 +0100

    Set up calculation propagation from nbBox moving to opBox
    - Move the activateRaycast function to box script (from op box)
    - Added a member var array with all direction raycasts
    - Added base logic to number to propagate the information of it's movement to op boxes adjacent to it's it's new position
    - Fixed a bug to the raycasts collision verification
    - Added a delay on the onMove signal emition after box movement, because without collision would sometimes not be detected (I think there a better solution to find)
    - Miscelanneous test in testing scene

--- Scripts/box.gd
+++ Scripts/box.gd
@@ -1,8 +1,11 @@
 @tool
 extends Node2D
 
-#region ===== VARAIBLES INITIALISATION ======
+#region ===== VARIABLES INITIALISATION ======
+
+#region === SIGNALS ===
 signal moved(direction : Vector2)
+#endregion
 
 #region === DISPLAY ===
 @onready var labelDisplay: Label = $valueDisplay
@@ -10,20 +13,27 @@ var displayValue : String
 #endregion
 
 #region === RAYCASTS ===
-@onready var leftNbBoxRaycast: ShapeCast2D = %leftNbBoxRaycast
-@onready var rightNbBoxRaycast: ShapeCast2D = %rightNbBoxRaycast
-@onready var upNbBoxRaycast: ShapeCast2D = %upNbBoxRaycast
-@onready var downNbBoxRaycast: ShapeCast2D = %downNbBoxRaycast
+@onready var leftBoxRaycast: ShapeCast2D = %leftBoxRaycast
+@onready var rightBoxRaycast: ShapeCast2D = %rightBoxRaycast
+@onready var upBoxRaycast: ShapeCast2D = %upBoxRaycast
+@onready var downBoxRaycast: ShapeCast2D = %downBoxRaycast
 
 # Order is important, fror operation like (left - right), (up / down)
-@onready var verticalRaycasts : Array[ShapeCast2D] = [upNbBoxRaycast, downNbBoxRaycast]
-@onready var horizontalRaycasts : Array[ShapeCast2D] = [leftNbBoxRaycast, rightNbBoxRaycast]
+@onready var verticalRaycasts : Array[ShapeCast2D] = [upBoxRaycast, downBoxRaycast]
+@onready var horizontalRaycasts : Array[ShapeCast2D] = [leftBoxRaycast, rightBoxRaycast]
+@onready var allRaycasts : Array[ShapeCast2D] = [leftBoxRaycast, upBoxRaycast, rightBoxRaycast, downBoxRaycast]
 
 var checkDirection : Array[ShapeCast2D]
 #endregion
+
 #endregion
 
 #region FUNCTIONS
+
+# Force the update of all the raycast in the array passed as arg
+func activateRaycast(raycasts : Array[ShapeCast2D]) -> void :
+	for raycast in raycasts:
+		raycast.force_shapecast_update()
 
 # Update the displayed value of the box
 func UpdateLabel() -> void:
@@ -36,6 +46,9 @@ func move(direction, length):
 	## length : int, length of the move in pixel
 	
 	position += direction * length
+	
+	# Wait a ms before emiting the signal, to be sure that the moved box won't be missed by other raycast it might trigger
+	await get_tree().create_timer(.01).timeout 
 	
 	moved.emit(direction)
 	
